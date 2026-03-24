4230a55f2c7c03234e5e705d3d40dfed98b4bc9c
Max Bergqvist <maxbergqvist@Maxs-MacBook-Pro.local>
2026-03-24 20:10:48 +0100

    Added table to handle connection between users and locations to normilize data

--- database/migrations/2026_03_24_000004_create_favorite_locations.php
+++ database/migrations/2026_03_24_000004_create_favorite_locations.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-      schema::create('locations', function (Blueprint $table) {
+      schema::create('favorite_locations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('location_id')->constrained()->onDelete('cascade');
